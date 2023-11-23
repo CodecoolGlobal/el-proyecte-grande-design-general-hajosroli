7c6978b8f043b87d996e224b2a5d465ebfa9be27
dorian-sz <dorian.szatmari@gmail.com>
2023-11-23 10:37:06 +0100

    Fix cors issue

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -15,15 +15,15 @@ return [
     |
     */
 
-    'paths' => ['api/', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => [''],
+    'allowed_methods' => ['*'],
 
     'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:8080')],
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => [''],
+    'allowed_headers' => ['*'],
 
     'exposed_headers' => [],
 
