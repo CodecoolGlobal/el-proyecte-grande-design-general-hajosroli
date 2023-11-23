efdb9658eccd1d3d3868910de5ffe40dc607fc39
dorian-sz <dorian.szatmari@gmail.com>
2023-11-23 11:10:52 +0100

    Add disable button on submit

--- frontend/src/components/AuthPage/RegistrationForm.jsx
+++ frontend/src/components/AuthPage/RegistrationForm.jsx
@@ -4,7 +4,7 @@ import { useAxios } from '../../hooks/useAxios';
 
 const RegistrationForm = () => {
     const [formData, setFormData] = useState({
-        'username': '',
+        'name': '',
         'email': '',
         'password': '',
     })
@@ -14,13 +14,15 @@ const RegistrationForm = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setIsLoading(true);
         const response = await axiosPost('/api/Register', formData);
+        setIsLoading(false);
         console.log(response); 
     }
 
     return (
         <form onSubmit={handleSubmit} className='flex gap-y-4 flex-col'>
-            <FloatingLabelInput id={"username"} label={"Username"} type={"text"} setFormData={setFormData} formData={formData} isLoading={isLoading}/>
+            <FloatingLabelInput id={"name"} label={"Name"} type={"text"} setFormData={setFormData} formData={formData} isLoading={isLoading}/>
             <FloatingLabelInput id={"email"} label={"Email address"} type={"email"} setFormData={setFormData} formData={formData} isLoading={isLoading}/>
             <FloatingLabelInput id={"password"} label={"Password"} type={"password"} setFormData={setFormData} formData={formData} isLoading={isLoading}/>
             <button type='submit' disabled={isLoading} className='bg-purple-900 rounded-md p-2 text-white font-bold disabled:opacity-75'>Register</button>
