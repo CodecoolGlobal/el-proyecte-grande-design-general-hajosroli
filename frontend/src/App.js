718687cba1fc6d992854e4d2132676ff76b53195
dorian-sz <dorian.szatmari@gmail.com>
2023-11-24 13:47:31 +0100

    Add protected route for demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import AuthenticationPage from './pages/AuthenticationPage';
 import MainLayout from './outlet/MainLayout';
 import HomePage from './pages/HomePage';
+import AuthLayout from './outlet/AuthLayout';
+import ProtectedPage from './pages/ProtectedPage';
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
         <Route path='/authentication' element={<AuthenticationPage />} />
         <Route element={<MainLayout />}>
           <Route index path='/' element={<HomePage />} />
+          <Route element={<AuthLayout />}>
+            <Route path='/protected' element={<ProtectedPage />}/>
+          </Route>
         </Route>
       </Routes>
   );
---
+++ frontend/src/pages/ProtectedPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProtectedPage = () => {
+  return (
+    <div>Example protected page!!!</div>
+  )
+}
+
+export default ProtectedPage
